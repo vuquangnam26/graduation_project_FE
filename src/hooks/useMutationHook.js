0f0d61170a4a4018a97d903b99ac73c8db25b35f
Nam2002 <movu171080@gmail.com>
2024-07-02 11:22:31 +0700

    d

--- src/hooks/useMutationHook.js
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query"
-
-export const useMutationHooks = (fnCallback) => {
-    const mutation = useMutation({
-        mutationFn: fnCallback
-    })
-    return mutation
-}
--- src/services/auth/authHeader.js
+++
@@ -1,5 +0,0 @@
-export const createAuthHeader = (tokenc) => {
-  return {
-    token: `Bearer ${tokenc}`,
-  };
-};
