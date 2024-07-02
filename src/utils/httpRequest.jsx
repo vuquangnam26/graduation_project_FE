4af22ef932b9662d6cbfc7a1956a6386d6c53db3
Nam2002 <movu171080@gmail.com>
2024-07-02 22:52:14 +0700

    cf

--- src/utils/httpRequest.jsx
+++ src/utils/httpRequest.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const req = axios.create({
-  baseURL: `https://testda-2.onrender.com`,
+  baseURL: `${process.env.REACT_API_URL_BACKEND}`,
   withCredentials: true,
 });
 
 export const axiosJWT = axios.create({
-  baseURL: `https://testda-2.onrender.com`,
+  baseURL: `${process.env.REACT_API_URL_BACKEND}`,
   withCredentials: true,
 });
