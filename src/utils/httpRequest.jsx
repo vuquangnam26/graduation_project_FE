2e2164003ed4f3cbecfe3f39abe026a1b43813c7
Nam2002 <movu171080@gmail.com>
2024-07-02 23:06:09 +0700

    conssol

--- src/utils/httpRequest.jsx
+++ src/utils/httpRequest.jsx
@@ -4,6 +4,8 @@ export const req = axios.create({
   baseURL: `${process.env.REACT_API_URL_BACKEND}`,
   withCredentials: true,
 });
+console.log(import.meta.env);
+console.log(process.env);
 
 export const axiosJWT = axios.create({
   baseURL: `${process.env.REACT_API_URL_BACKEND}`,
